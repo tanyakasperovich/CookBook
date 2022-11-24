bd22791afee58899c476aff23a488daa7324b761
Tatsiana Kaspiarovich <tanyakasperovich@icloud.com>
2022-11-24 12:25:09 +0300

    Initial Commit

---
+++ CookBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CookBook
+//
+//  Created by Татьяна Касперович on 24.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Татьяна Касперович on 24.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
